b113594415c900e17d89919351dfcd844fda8a0b
Victor Lin <victor@victorl.in>
2021-01-12 10:50:31 -0500

    put parsing logic in separate file

--- src/Summary.py
+++ src/Summary.py
@@ -1,5 +1,6 @@
 import io
 from summary_download import get_summary_text
+from summary_parse import parse
 
 class Summary(object):
 
@@ -11,53 +12,7 @@ class Summary(object):
     def __init__(self, run_id):
         self.run_id = run_id
         summary_text = get_summary_text(run_id)
-        self._parse(io.StringIO(summary_text))
-
-    def _parse(self, fs):
-        comment_line = next(fs)
-        self.properties = parse_comment_line(comment_line)
-        self.run_id = self.properties['sra']
-        line = next(fs)
-        self.families = []
-        while line.startswith('famcvg'):
-            d = parse_family_line(line)
-            d['run'] = self.run_id
-            self.families.append(d)
-            line = next(fs)
-        self.sequences = []
-        while line.startswith('seqcvg'):
-            d = parse_sequence_line(line)
-            d['run'] = self.run_id
-            self.sequences.append(d)
-            try:
-                line = next(fs)
-            except StopIteration:
-                break
+        parse(self, io.StringIO(summary_text))
 
     def __str__(self):
         return self.run_id
-
-
-def parse_comment_line(line):
-    return dict([pair.split('=') for pair in
-        line.replace('SUMZER_COMMENT=', '')
-        .rstrip(';\n')
-        .replace(';', ',')
-        .split(',')])
-
-
-def parse_generic_line(line):
-    return dict([pair.split('=') for pair in line.rstrip(';\n').split(';')])
-
-
-def parse_family_line(line):
-    return parse_generic_line(line)
-
-
-def parse_sequence_line(line):
-    # there can be ';' and '=' in the last entry (name)
-    name_index = line.index('name=')
-    d1 = parse_generic_line(line[:name_index])
-    d2 = dict([line[name_index:].strip(';\n').split('=', maxsplit=1)])
-    d1.update(d2)
-    return d1
