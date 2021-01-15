478f3b1fcd390f0219b93c087f136425a6381d40
Victor Lin <victor@victorl.in>
2021-01-15 12:11:32 -0500

    add more info in lambda return

--- src/lambda_single.py
+++ src/lambda_single.py
@@ -1,10 +1,17 @@
+import time
 from Summary import Summary
 
 def handler(event, context):
+    start_time = time.time()
     run_id = event['run']
     print(run_id)
     summary = Summary(run_id)
     if (summary.already_uploaded()):
         return { 'message' : f'{run_id} already processed' }
     summary.upload()
-    return { 'message' : f'{run_id} done processing' }
+    return {
+        'run' : run_id,
+        'time' : time.time() - start_time,
+        'families' : len(summary.families),
+        'sequences' : len(summary.sequences)
+    }
