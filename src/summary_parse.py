58c3ebd245893a2372daa154b840d46f90f38193
Victor Lin <victor@victorl.in>
2021-01-15 21:16:58 -0500

    fix family line parsing

--- src/summary_parse.py
+++ src/summary_parse.py
@@ -33,7 +33,12 @@ def parse_generic_line(line):
     return dict([pair.split('=') for pair in line.rstrip(';\n').split(';')])
 
 def parse_family_line(line):
-    return parse_generic_line(line)
+    # there can be ';' and '=' in the last entry (topname)
+    name_index = line.index('topname=')
+    d1 = parse_generic_line(line[:name_index])
+    d2 = dict([line[name_index:].strip(';\n').split('=', maxsplit=1)])
+    d1.update(d2)
+    return d1
 
 def parse_sequence_line(line):
     # there can be ';' and '=' in the last entry (name)
