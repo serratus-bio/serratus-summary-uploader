a831d645d0b65f477a6e070a5787648e104712d9
Victor Lin <victor@victorl.in>
2021-01-17 22:58:53 -0500

    implement 2nd degree parallelism

--- src/lambda_batch/main.py
+++ src/lambda_batch/main.py
@@ -8,21 +8,50 @@ WORKER_LAMBDA = os.environ['WORKER_LAMBDA']
 INDEX_BUCKET = os.environ['INDEX_BUCKET']
 INDEX_FILE = os.environ['INDEX_FILE']
 MINIMUN_REMAINING_TIME_MS = 10000
+MAX_LINES_PER_BRANCH = 1000
 s3_object = s3.Object(bucket_name=INDEX_BUCKET, key=INDEX_FILE)
 
-seed_event_keys = {
-    'start_byte'
+branch_event_keys = {
+    'start_byte',
+    'end_byte'
 }
 
 def handler(event, context):
-    if set(event) == seed_event_keys:
+    if set(event) == set():
         return seed_handler(event, context)
+    if set(event) == branch_event_keys:
+        return branch_handler(event, context)
     else:
         raise ValueError('Invalid event keys.')
 
 def seed_handler(event, context):
     start_byte = event.get('start_byte', 0)
     response = s3_object.get(Range=f'bytes={start_byte}-')
+    next_start_byte = start_byte
+    n_lines = 0
+    for line in response['Body'].iter_lines():
+        run_id = line.decode('utf-8')
+        n_lines += 1
+        next_start_byte += len(line) + 1  # \n
+        if n_lines > MAX_LINES_PER_BRANCH:
+            break
+        if context.get_remaining_time_in_millis() < MINIMUN_REMAINING_TIME_MS:
+            break
+
+    # invoke branch
+    invoke_lambda(context.function_name, { 'start_byte': start_byte, 'end_byte': next_start_byte - 1 })
+    if next_start_byte < s3_object.content_length:
+        invoke_lambda(context.function_name, { 'start_byte': next_start_byte })
+    log = {
+        'n_lines': n_lines
+    }
+    print(log)
+    return log
+
+def branch_handler(event, context):
+    start_byte = event['start_byte']
+    end_byte = event['end_byte']
+    response = s3_object.get(Range=f'bytes={start_byte}-{end_byte}')
     n_workers_invoked = 0
     next_start_byte = start_byte
     for line in response['Body'].iter_lines():
@@ -36,17 +65,15 @@ def seed_handler(event, context):
         if context.get_remaining_time_in_millis() < MINIMUN_REMAINING_TIME_MS:
             break
 
-    if next_start_byte < s3_object.content_length:
-        new_event = {
-            'start_byte': next_start_byte
-        }
-        invoke_lambda(context.function_name, new_event)
-    return {
-        'content_length': s3_object.content_length,
+    if next_start_byte < end_byte:
+        invoke_lambda(context.function_name, { 'start_byte': next_start_byte, 'end_byte': end_byte })
+    log = {
         'start_byte': start_byte,
+        'end_byte': end_byte,
         'next_start_byte': next_start_byte,
         'n_workers_invoked'  : n_workers_invoked
     }
+    print(log)
 
 # https://medium.com/swlh/processing-large-s3-files-with-aws-lambda-2c5840ae5c91
 def invoke_lambda(function_name, event):
