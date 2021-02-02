98bc4d3bbdce34e78eccb875b2906e168491de99
Victor Lin <victor@victorl.in>
2021-02-02 10:27:37 -0500

    rename single,batch -> worker,manager

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 cd src
 
-pushd lambda_worker
+pushd worker
 zip -rq package .
 aws lambda update-function-code \
-    --function-name serratus-summary-uploader-single \
+    --function-name serratus-summary-uploader-worker \
     --zip-file fileb://./package.zip
 rm package.zip
 popd
 
-pushd lambda_batch
+pushd manager
 zip -rq package .
 aws lambda update-function-code \
-    --function-name serratus-summary-uploader-batch \
+    --function-name serratus-summary-uploader-manager \
     --zip-file fileb://./package.zip
 rm package.zip
 popd
