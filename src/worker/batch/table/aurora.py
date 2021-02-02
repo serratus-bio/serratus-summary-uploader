a0aee3d8168b652d01814e9b5c276cddfdd57b40
Victor Lin <victor@victorl.in>
2021-02-02 11:28:50 -0500

    add retry for aurora data api

--- src/worker/batch/table/aurora.py
+++ src/worker/batch/table/aurora.py
@@ -1,7 +1,9 @@
 from . import UploadTable
 from sqlalchemy import create_engine
+from sqlalchemy.exc import StatementError
 import boto3
 import pandas as pd
+import time
 
 cluster_arn = 'arn:aws:rds:us-east-1:797308887321:cluster:serratus-aurora'
 secret_arn = 'arn:aws:secretsmanager:us-east-1:797308887321:secret:rds-db-credentials/cluster-KOFPN4Q2TKDBO5FHY6QO5M3S7Q/serratus-agdBn9'
@@ -25,6 +27,18 @@ class AuroraTable(UploadTable):
             return
         engine = create_engine('postgresql+auroradataapi://:@/summary',
             connect_args=dict(aurora_cluster_arn=cluster_arn, secret_arn=secret_arn))
+        retry_count = 0
+        while retry_count < 5:
+            try:
+                return self.try_upload(engine)
+            except StatementError as e:  # botocore.errorfactory.StatementTimeoutException
+                if not 'StatementTimeoutException' in str(e):
+                    raise e
+                retry_count += 1
+                time.sleep(2 ** retry_count)
+        return self.try_upload(engine)
+
+    def try_upload(self, engine):
         with engine.connect() as con:
             self.df.to_sql(self.name, con,
                 if_exists='append',
